e7484cec8b8db9bb63b298347bf443c8ae544533
suryaadiwiguna <suryaadiwiguna@gmail.com>
2023-12-30 23:22:52 +0700

    Feat: Add new product hit API

--- components/AddNewProduct.tsx
+++ components/AddNewProduct.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import {
     Modal,
     ModalOverlay,
@@ -9,23 +10,62 @@ import {
     ModalCloseButton,
     useDisclosure,
     Button,
-    Text,
     FormControl,
     FormLabel,
-    FormHelperText,
     FormErrorMessage,
     Input,
-    Box,
-    Flex,
     Textarea,
     Select,
-    Stack
+    Stack,
 } from '@chakra-ui/react'
 import { AddIcon } from '@chakra-ui/icons'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 export default function AddNewProduct() {
 
+    //Modal handler (Chakra UI)
     const { isOpen, onOpen, onClose } = useDisclosure()
+
+    //Formik (Form handler)
+    const formik = useFormik({
+        initialValues: {
+            Product_Name: '',
+            Price: '',
+            Stock: '',
+            Product_Type_Code: '',
+            Product_Description: '',
+            Created_By: 'Surya'
+        },
+        validationSchema: Yup.object({
+            Product_Name: Yup.string()
+                .min(2, "Must be at least 2 characters")
+                .required("Product name is required"),
+            Price: Yup.number()
+                .positive()
+                .moreThan(0)
+                .required("Price is required"),
+            Stock: Yup.number()
+                .required('Stock is required'),
+            Product_Type_Code: Yup.string()
+                .required("Product type is required"),
+            Product_Description: Yup.string()
+        }),
+        onSubmit: (values, { setSubmitting }) => {
+            console.log(values)
+            axios.post('https://idrus-haerulumam.outsystemscloud.com/JagaWaroeng_API/rest/JagaWaroeng_API/Insert_Product', values)
+                .then((res: AxiosResponse) => {
+                    alert(res.data.msg)
+                    console.log(res)
+                    setSubmitting(false)
+                    onClose()
+                })
+                .catch((error: AxiosError) => {
+                    alert(error)
+                })
+        }
+    })
 
     return (
         <>
@@ -33,82 +73,86 @@ export default function AddNewProduct() {
 
             <Modal isOpen={isOpen} onClose={onClose} size={'4xl'}>
                 <ModalOverlay />
-                <ModalContent mx={'1em'}>
+                <ModalContent>
                     <ModalHeader>Add New Product</ModalHeader>
                     <ModalCloseButton />
+                    <form onSubmit={formik.handleSubmit}>
+                        <ModalBody>
+                            <Stack spacing={3}>
+                                <FormControl isRequired isInvalid={Boolean(formik.touched.Product_Name && formik.errors.Product_Name)}>
+                                    <FormLabel htmlFor='Product_Name'>Product Name</FormLabel>
+                                    <Input
+                                        id='Product_Name'
+                                        name='Product_Name'
+                                        type='text'
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.Product_Name}
+                                    />
+                                    <FormErrorMessage>{formik.errors.Product_Name}</FormErrorMessage>
+                                </FormControl>
 
-                    <ModalBody>
-                        <Stack spacing={3}>
-                            <FormControl isRequired>
-                                <FormLabel>Product Name</FormLabel>
-                                <Input type='text' placeholder='New product name' />
-                                <FormErrorMessage>Product name is required.</FormErrorMessage>
-                            </FormControl>
-                            <FormControl isRequired>
-                                <FormLabel>Price</FormLabel>
-                                <Input type='number' placeholder='The price (number only)' />
-                                <FormErrorMessage>Price is required.</FormErrorMessage>
-                            </FormControl>
-                            <FormControl isRequired>
-                                <FormLabel>Stock</FormLabel>
-                                <Input type='number' placeholder='Stock (number only)' />
-                                <FormErrorMessage>Stock is required.</FormErrorMessage>
-                            </FormControl>
-                            <FormControl isRequired>
-                                <FormLabel>Product Type</FormLabel>
-                                <Select placeholder='Select product type'>
-                                    <option value={'MKN'}>Food</option>
-                                    <option value={'RKK'}>Cigarette</option>
-                                    <option value={'MNM'}>Beverage</option>
-                                </Select>
-                            </FormControl>
-                            <FormControl >
-                                <FormLabel>Description</FormLabel>
-                                <Textarea placeholder='Product description' />
-                            </FormControl>
-                        </Stack>
-                    </ModalBody>
+                                <FormControl isRequired isInvalid={Boolean(formik.touched.Price && formik.errors.Price)}>
+                                    <FormLabel htmlFor='Price'>Price</FormLabel>
+                                    <Input
+                                        id='Price'
+                                        name='Price'
+                                        type='text'
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.Price}
+                                    />
+                                    <FormErrorMessage>{formik.errors.Price} </FormErrorMessage>
+                                </FormControl>
 
-                    <ModalFooter>
-                        <Button colorScheme='orange' variant={'outline'} mr={3} onClick={onClose}> Close </Button>
-                        <Button colorScheme='orange' variant={'solid'} onClick={handleSubmit} type='submit'> Submit </Button>
-                    </ModalFooter>
+                                <FormControl isRequired isInvalid={Boolean(formik.touched.Product_Type_Code && formik.errors.Product_Type_Code)}>
+                                    <FormLabel>Product Type</FormLabel>
+                                    <Select
+                                        placeholder='Select product type'
+                                        id='Product_Type_Code'
+                                        name='Product_Type_Code'
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.Product_Type_Code}
+                                    >
+                                        <option value={'MKN'}>Food</option>
+                                        <option value={'RKK'}>Cigarette</option>
+                                        <option value={'MNM'}>Beverage</option>
+                                    </Select>
+                                    <FormErrorMessage> {formik.errors.Product_Type_Code} </FormErrorMessage>
+                                </FormControl>
 
+                                <FormControl isRequired isInvalid={Boolean(formik.touched.Stock && formik.errors.Stock)}>
+                                    <FormLabel>Stock</FormLabel>
+                                    <Input
+                                        id='Stock'
+                                        name='Stock'
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.Stock}
+                                    />
+                                    <FormErrorMessage> {formik.errors.Stock} </FormErrorMessage>
+                                </FormControl>
+                                <FormControl isInvalid={Boolean(formik.touched.Product_Description && formik.errors.Product_Description)}>
+                                    <FormLabel>Description</FormLabel>
+                                    <Textarea
+                                        id='Product_Description'
+                                        name='Product_Description'
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.Product_Description}
+                                    />
+                                </FormControl>
+                            </Stack>
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button px={'2em'} onClick={onClose}>Cancel</Button>
+                            <Button px={'2em'} ml={'1em'} colorScheme='orange' type='submit' isLoading={formik.isSubmitting}>Submit</Button>
+                        </ModalFooter>
+                    </form>
                 </ModalContent>
-            </Modal>
+            </Modal >
         </>
     )
-}
-
-function NewProductForm() {
-    return (
-        <>
-            <Flex direction={'column'} gap={4}>
-                <FormControl isRequired>
-                    <FormLabel>Product Name</FormLabel>
-                    <Input type='text' placeholder='New product name' />
-                    <FormErrorMessage>Product name is required.</FormErrorMessage>
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel>Price</FormLabel>
-                    <Input type='number' placeholder='The price (number only)' />
-                    <FormErrorMessage>Price is required.</FormErrorMessage>
-                </FormControl>
-                <FormControl >
-                    <FormLabel>Description</FormLabel>
-                    <Textarea placeholder='Product description' />
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel>Stock</FormLabel>
-                    <Input type='number' placeholder='Stock (number only)' />
-                    <FormErrorMessage>Stock is required.</FormErrorMessage>
-                </FormControl>
-            </Flex>
-        </>
-
-    )
-}
-
-function handleSubmit() {
-
 }
