bdee7a8a76d48eeb06ca6124fe651f6c16cd55ca
suryaadiwiguna <suryaadiwiguna@gmail.com>
2024-01-01 06:54:34 +0700

    env to NEXT_PUBLIC_

--- next.config.js
+++ next.config.js
@@ -6,8 +6,7 @@ module.exports = nextConfig
 module.exports = {
     reactStrictMode: true,
     env: {
-        BASE_URL: process.env.BASE_URL,
-        GET_PRODUCT: process.env.GET_PRODUCT,
-        GET_PRODUCT_LIST: process.env.GET_PRODUCT_LIST
+        GET_PRODUCT: process.env.NEXT_PUBLIC_GET_PRODUCT,
+        GET_PRODUCT_LIST: process.env.NEXT_PUBLIC_GET_PRODUCT_LIST
     }
 }
